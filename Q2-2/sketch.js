11a517a2a93073aa77cf269314521dd8a6a83d0c
koibuchi03-tech <koibuchi.03@gmail.com>
2025-11-25 10:59:15 +0900

    final

--- Q2-2/sketch.js
+++ Q2-2/sketch.js
@@ -6,6 +6,8 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
+      fill(225,0,0);
+      rect(x,x,20,20);
     }
   }
 }
